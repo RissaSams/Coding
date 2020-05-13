e1ff10a295c7582616ddefbeb9458d53377cd78a
RissaSams <rissa.sams@yahoo.com>
2020-05-13 09:30:28 -0500

    Add project files.

---
+++ Hands On.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Coding
+{
+    class Hands_On
+    {
+        static void Main(string[] args)
+        {
+
+            //AverageAnswer();
+            //MultiAnswer();
+            
+        }
+
+        static void MultiAnswer()
+        {
+            Console.Write("Enter a #: ");
+            string str = Console.ReadLine();
+            int x;
+            Int32.TryParse(str, out x);
+            MultiplicationTable(x);
+
+        }
+
+        static void MultiplicationTable(int num)
+        {
+            Console.Write("    |");
+            for (int i = 0; i <= num; i++)
+            {
+                Console.Write($"{i,4}");
+            }
+            Console.WriteLine();
+            Console.Write("----+");
+            for (int i = 0; i <= num; i++)
+            {
+                Console.Write($"----");
+            }
+            Console.WriteLine();            
+            for (int i = 0; i <= num; i++)
+            {
+                Console.Write($"{i,3} |");
+                for (int j = 0; j <= num; j++)
+                {
+                    Console.Write($"{i*j,4}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void AverageAnswer()
+        {
+            
+            int[] array1 = { 1, 4, 6, 1, 3, 9, 0 , -1, -6, 11};
+            int[] array2 = { -9, -7 -7, -11, -7, -88, -5, -10 };
+            double ave;
+            
+            ave = FindAverage(array1);
+            Console.WriteLine("The avarge of the array is: " + ave);
+
+            ave = FindAverage(array2);
+            Console.WriteLine("The avarge of the array is: " + ave);
+            
+
+        }
+
+        static double FindAverage(int[] arNums)
+        {
+            double average, total = 0; ;
+
+            for (int i = 0; i < arNums.Length; i++)
+            {
+                total = arNums[i];
+                Console.WriteLine("The value is: " + total);
+            }
+                        
+            /*foreach (int num in arNums)
+            {
+                total += num;
+            }
+            */
+
+            // average = arNums.Average();
+
+            average = total / arNums.Length;
+            return average;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Coding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userProblem;
+            double num1, num2, total;
+            while (true)
+            {
+                userProblem = GetInput("Enter a math problem or exit: ");
+                if (userProblem == "exit")
+                {
+                    break;
+                }
+                string[] userProblemsParts = userProblem.Split(" ");
+                Double.TryParse(userProblemsParts[0], out num1);
+                Double.TryParse(userProblemsParts[2], out num2);
+
+                if (userProblemsParts[1] == "+")
+                {
+                    total = num1 + num2;
+                    Console.WriteLine("The total is: " + total);
+                }
+                else if (userProblemsParts[1] == "-")
+                {
+                    total = num1 - num2;
+                    Console.WriteLine("The total is: " + total);
+                }
+                else if (userProblemsParts[1] == "*")
+                {
+                    total = num1 * num2;
+                    Console.WriteLine("The total is: " + total);
+                }
+                else if (userProblemsParts[1] == "/")
+                {
+                    total = num1 / num2;
+                    Console.WriteLine("The total is: " + total);
+                }
+                else if (userProblemsParts[1] == "^")
+                {
+                    total = Math.Pow(num1, num2);
+                    Console.WriteLine("The total is: " + total);
+                }
+                else if (userProblemsParts[1] == "%")
+                {
+                    total = num1 % num2;
+                    Console.WriteLine("The total is: " + total);
+                }
+            }
+        }
+
+        public static string GetInput(string prompt)
+        {
+            Console.Write(prompt);
+            string str = Console.ReadLine();
+            return str;
+        }   // end of the GetInput method
+
+        public static int GetNumber(string prompt)
+        {
+            int userNumber;
+            string strNumber = GetInput(prompt);
+            while (!Int32.TryParse(strNumber, out userNumber))
+            {
+                Console.WriteLine("That is not an integer");
+                strNumber = GetInput(prompt);
+            }
+            return userNumber;
+        }   // end of the GetNumber method
+    }
+}
